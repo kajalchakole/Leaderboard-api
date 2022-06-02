2c44b9f8e8a5c39d18f1c711ca51e75460605311
Kajal Chakole <kajal.chakole@tadigital.com>
2022-06-02 14:48:39 +0530

    Added default values for Limit and Sort Field

--- services/TopPerformersService.js
+++ services/TopPerformersService.js
@@ -2,10 +2,31 @@ const Agent = require('../models/Agent');
 const AgentCallDetails = require('../models/AgentCallDetails');
 
 module.exports = class TopPerformersService {
-    static async getTopPerformers({field, limit}) {
+    static async getTopPerformers({ field="totalIncentives", limit=5 }) {
         try {
-            let param = "-"+field;
-            const records = await Agent.find().sort(param).limit(limit); 
+            let param = "-" + field;
+            return await Agent.find().sort(param).limit(limit);
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    static async getTopPerformersByCategory({ field }) {
+        try {
+            const records = await AgentCallDetails.aggregate([
+                {
+                    $group: {
+                        _id: "$empId",
+                        totalIncentives: { $sum: "$incentives" }
+                    }
+                },
+                {
+                    $sort:
+                    {
+                        totalIncentives: -1
+                    }
+                }
+            ]);
             return records;
         } catch (error) {
             throw new Error(error);
